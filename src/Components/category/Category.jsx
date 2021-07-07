840f3949bc6a7c26eca00f05308a867563d4bbb8
Robiya <tuhtaevarobiahon@gmail.com>
2021-07-07 14:31:13 +0500

    initial commit

--- src/Components/category/Category.jsx
+++ src/Components/category/Category.jsx
@@ -29,7 +29,7 @@ const Category = () => {
                     </div>
                 </div>
                 <div className={style.Category_Shop}>
-                    <Route path='/' exact component={Carousel} />
+                    <Route path='/main'  component={Carousel} />
                     <Route path='/telefon' component={Telefon} />
                     <Route path='/accessories' component={Aksesuar} />
                     <Route path='/kompiyuter' component={kompiyuter} />
